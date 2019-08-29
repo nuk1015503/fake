32602e82ab7fa9dc0c35ee00ee5e2fb132c32b39
hunter <nuk1015503@gmail.com>
2019-08-29 16:24:05 +0800

    add auto delete data

--- main.go
+++ main.go
@@ -11,12 +11,38 @@ import (
 )
 
 var dataMap sync.Map
+var nextDelTime string
 
 func main() {
 	port := os.Getenv("PORT")
 	http.HandleFunc("/data", dataHandle)
+	http.HandleFunc("/delTime", delTimeHandle)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "provide fake data ！！")
+		fmt.Fprintf(w, `
+		provide fake data ！！
+		router:[/data]
+		method:[POST] {
+			header:[key] or url parameter[key]
+			body:what you want to get
+		}
+
+		method:[GET] {
+			header:[key] or url parameter[key]
+			reponse:what you post by key
+		}
+
+		method:[delete] {
+			header:[key] or url parameter[key]
+			reponse:delete data by [key]
+		}
+
+		======================================
+		router:[/delTime]
+		method:[GET] {
+			reponse:next delete time
+		}
+
+		`)
 	})
 	if port == "" {
 		port = "8888"
@@ -31,10 +57,16 @@ func clearMap() {
 			dataMap.Delete(key)
 			return true
 		})
+		now := time.Now().Add(24 * time.Hour)
+		nextDelTime = now.Format("2006/01/02/ 15:04")
 		time.Sleep(24 * time.Hour)
 	}
 }
-
+func delTimeHandle(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		w.Write([]byte("next delete time : " + nextDelTime))
+	}
+}
 func dataHandle(w http.ResponseWriter, r *http.Request) {
 	keys := r.URL.Query()
 	key := keys.Get("key")
